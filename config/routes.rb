238c2d915b20c9e2ad2390e17d3c466147bf51cd
zeesa1802 <zeesa18.2@gmail.com>
2022-03-12 17:30:48 +0500

    completed the comment form for each articles.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   root "articles#index"
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   get '/posts', to: 'posts#index'
   # root to: "employees#index"
